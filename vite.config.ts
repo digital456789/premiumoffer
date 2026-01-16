23796c1ae4d01ae1db54bf0b56d8d1c0d45c79c2
digital456789 <79628482+digital456789@users.noreply.github.com>
2026-01-16 18:04:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/premiumoffer/', // Yeh aapki repository ka naam hai
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
